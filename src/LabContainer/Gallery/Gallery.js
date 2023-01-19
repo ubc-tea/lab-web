f6495ae31e0ef766468fda20283414da804cbd3f
Robin Lai <robinlaiyy@gmail.com>
2023-01-18 18:41:42 -0800

    add gallery button and Carousel effect

--- src/LabContainer/Gallery/Gallery.js
+++ src/LabContainer/Gallery/Gallery.js
@@ -6,6 +6,43 @@ export default function Gallery(props) {
     return (
         <div className="gal-parent" id={props.id || ""}>
             <ScreenHeading title={"Gallery of TEA Group"} subHeading={""} />
+            <div class="window">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100 galStyle" src={require("../../assets/Home/group_small.png")} alt="First slide"/>
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100 galStyle" src={require("../../assets/Home/tea_group2.jpg")} alt="Second slide"/>
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100 galStyle" src={require("../../assets/Home/tea_group1.jpg")} alt="Third slide"/>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <img width="32" height="32" src={require("../../assets/Home/left.png")} />
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <img width="32" height="32" src={require("../../assets/Home/right.png")} />
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+{/* <div className="gal-parent" id={props.id || ""}>
+            <ScreenHeading title={"Gallery of TEA Group"} subHeading={""} />
             <div class="gal-container">
                 <div>
                     <img src={require("../../assets/Home/group.JPG")} width="300" height="200" class="items" />
@@ -17,6 +54,4 @@ export default function Gallery(props) {
                     <img src={require("../../assets/Home/tea_group1.jpg")} width="300" height="200" class="items" />
                 </div>                
             </div>
-        </div>
-    )
-}
+        </div> */}
