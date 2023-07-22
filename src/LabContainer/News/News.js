1c975c3d8f506a06e2d4da95aae028d89e170ea3
TeaLab <xiaoxiao.li@aya.yale.edu>
2023-07-21 23:34:08 -0700

    Update News.js

--- src/LabContainer/News/News.js
+++ src/LabContainer/News/News.js
@@ -15,7 +15,7 @@ export default function News(props) {
   const news = [
     {
       title:
-        "[06/2023] We got three papers accepted by MICCAI 2023.",
+        "[06/2023] We got three papers accepted by MICCAI 2023. Congrats, Minghui, Anushree, and Meirui!",
     },
     {
       title:
@@ -37,18 +37,18 @@ export default function News(props) {
       title:
         "[01/2023] Dr. Li is serving as a Program Chairs for MIDL 2023. Dr. Li is serving as an Area Chair and Organizing Committee (Women in MICCAI) for MICCAI 2023. Dr. Li was appointed as an Assitant Professor Adjunct at Yale University.",
     },
-    {
-      title:
-        "[12/2022] We received the Health Innovation Funding Investment (HIFI) Awards on deep learning-based cancer screening project.",
-    },
+    // {
+    //   title:
+    //     "[12/2022] We received the Health Innovation Funding Investment (HIFI) Awards on deep learning-based cancer screening project.",
+    // },
     {
       title:
         "[07/2022] We received Meta Research Award on the project of data auditing for ML, leading by Prof. Kai Li at Princeton.",
     },
-    {
-      title:
-        "[06/2022] Chun-Yin and Minghui were selected as Blockchain Graduate Pathway 2022-2023 fellows. Weina received Borelis AI fellowship.",
-    },
+    // {
+    //   title:
+    //     "[06/2022] Chun-Yin and Minghui were selected as Blockchain Graduate Pathway 2022-2023 fellows. Weina received Borelis AI fellowship.",
+    // },
     {
       title:
         "[04/2022] We received NSERC Discovery Grants program's support on continous learning project.",
@@ -65,8 +65,8 @@ export default function News(props) {
       title:
         "[03/2022] We received NVIDIA Academic Hardware Grant to continuously support our FL projects.",
     },
-    { title: "[02/2022] Dr.Li was selected as a Google Cloud Research Innovator." },
-    { title: "[11/2021] We received Health Innovation Funding Investment Awards with Dr. Charlene Ronquillo on fairness on healthcare data analysis project." },
+    // { title: "[02/2022] Dr.Li was selected as a Google Cloud Research Innovator." },
+    // { title: "[11/2021] We received Health Innovation Funding Investment Awards with Dr. Charlene Ronquillo on fairness on healthcare data analysis project." },
   ];
 
   const renderNews = () => {
