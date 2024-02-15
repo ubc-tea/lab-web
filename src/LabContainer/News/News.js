a50ed66590d8bc09d326f952e51bce1e81e084df
Beidi Zhao <57205014+mayonezu216@users.noreply.github.com>
2024-02-15 13:12:28 -0800

    Update News.js
    add 2/2024 MEDIMA paper

--- src/LabContainer/News/News.js
+++ src/LabContainer/News/News.js
@@ -15,6 +15,10 @@ export default function News(props) {
   const news = [
     {
       title:
+        "[02/2024] We have one paper accepted by Meidical Image Analysis. Congrats, Beidi!",
+    },
+    {
+      title:
         "[01/2024] We got one paper accepted by ICLR 2024 on Heterogenous Federated Learning!",
     },
     {
