5fe60bb43138b985933ea27d5bb2066261187477
TeaLab <xiaoxiao.li@aya.yale.edu>
2024-05-10 15:54:24 -0700

    Update Publications.js

--- src/LabContainer/Publications/Publications.js
+++ src/LabContainer/Publications/Publications.js
@@ -22,7 +22,7 @@ export default function Publications(props) {
     {
     title:
      "FedCal: Achieving Local and Global Calibration in Federated Learning via Aggregated Parameterized Scaler",
-    author: "Deng, W., Thrampoulidis, C., & Li, X.",
+    author: "Peng, H., Yu, H., Tang, X., & Li, X.",
     journal: "ICML 2024"
   },
     {
