a137c9c02d3ed79a7d9eddb88e0ae11b2c59623e
TeaLab <xiaoxiao.li@aya.yale.edu>
2024-11-09 08:20:23 -0800

    updates

--- src/LabContainer/Positions/Positions.js
+++ src/LabContainer/Positions/Positions.js
@@ -46,7 +46,8 @@ export default function News(props) {
         If you want to join TEA, please email to <a href="mailto:xiaoxiao.li@ece.ubc.ca">xiaoxiao.li@ece.ubc.ca</a>, including your CV, transcript, and one of your research papers if applicable. Due to the large amount of emails I receive, I may not be able to respond to each one individually. To help me notice you email, please put **[ILOLLEH]**, written backwards, in your email subject. Please strictly follow the instruction.
         </div>
         <div className="content">
-        <li><strong> Graduate Students</strong>: Multiple positions for graduate students are available for 2023.</li>
+        <li><strong> Postdoc</strong>: Posdtoc positions are available with flexible starting date.</li>
+        <li><strong> Graduate Students</strong>: Multiple positions for graduate students are available for 2025 Fall.</li>
         <li><strong>Intern Students and Visiting Students/Scholars</strong>: All formats of local and remote collaboration are welcomed. For the intern students at UBC, we need to work together through course registration or summer internship programs.</li>
         </div>
       </div>
