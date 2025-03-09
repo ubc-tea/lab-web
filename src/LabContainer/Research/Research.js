30236cd0c55a292868a7d81a1f8b8193a6ff2b8c
Nanboy-Ronan <jinruinan@163.com>
2025-03-08 20:26:02 -0800

    update

--- src/LabContainer/Research/Research.js
+++ src/LabContainer/Research/Research.js
@@ -49,32 +49,32 @@ export default function Research(props) {
       description: "Data valuation plays a crucial role in machine learning. Existing data valuation methods, mainly focused on discriminative models, overlook generative models that have gained attention recently. In generative models, data valuation measures the impact of training data on generated datasets. We formulate the data valuation problem in generative models from a similarity matching perspective to bridge the gaps. Specifically, we introduce GMValuator, , the first training-free and model-agnostic approach to providing data valuation for generation tasks.",
       imgurl:require("../../assets/Research/valuator.png"),
     },
-    {
-      title:
-        "Federated Learning on Non-iid Features",
-      description:
-        "Most of the previous federated learning work has focused on a difference in the distribution of labels. Unlike those settings, we address an important problem of FL, e.g., different scanner/sensors in medical imaging, where local clients may store examples with different marginal or conditional feature distributions compared to other nodes, which we denote as feature shift non-iid. In this work, we propose an effective method that uses local batch normalization to alleviate the feature shift before averaging models.",
-      imgurl: require("../../assets/Research/fedbn.png"),
-    },
-   {
-      title:
-        "Self-supervised Learning on Graph Data",
-      description:
-        "In population graph-based disease analysis, graph convolutional neural networks (GCNs) have achieved remarkable success. However, these achievements are inseparable from abundant labeled data and sensitive to spurious signals. To improve fMRI representation learning and classification under a label-efficient setting, we propose a novel and theory-driven self-supervised learning (SSL) framework on GCNs, called GATE. In this work, we focus on the challenging task, neuro-disease classification, using fMRI and esign suitable and effective SSL strategies to extract formation and robust features for fMRI.",
-      imgurl: require("../../assets/Research/gate.png"),
-    },
-    {
-      title: "Data-free Incremental Learning",
-      description: 
-      "A major challenge of incremental learning is catastrophic forgetting. To alleviate this problem, prior methodologies save a portion of training data that require perpetual storage, which may introduce privacy issues. Here, we propose a novel data-free class incremental learning framework that first synthesizes data from the model trained on previous classes to generate synthetic data, called Class Impression, which is used to update model with new data. With the assistance with serveral novel losses, our pipeline improves accuracy for class incremental learning.",
-      imgurl: require("../../assets/Research/class_impression.png"),
-    },
+    // {
+    //   title:
+    //     "Federated Learning on Non-iid Features",
+    //   description:
+    //     "Most of the previous federated learning work has focused on a difference in the distribution of labels. Unlike those settings, we address an important problem of FL, e.g., different scanner/sensors in medical imaging, where local clients may store examples with different marginal or conditional feature distributions compared to other nodes, which we denote as feature shift non-iid. In this work, we propose an effective method that uses local batch normalization to alleviate the feature shift before averaging models.",
+    //   imgurl: require("../../assets/Research/fedbn.png"),
+    // },
+  //  {
+  //     title:
+  //       "Self-supervised Learning on Graph Data",
+  //     description:
+  //       "In population graph-based disease analysis, graph convolutional neural networks (GCNs) have achieved remarkable success. However, these achievements are inseparable from abundant labeled data and sensitive to spurious signals. To improve fMRI representation learning and classification under a label-efficient setting, we propose a novel and theory-driven self-supervised learning (SSL) framework on GCNs, called GATE. In this work, we focus on the challenging task, neuro-disease classification, using fMRI and esign suitable and effective SSL strategies to extract formation and robust features for fMRI.",
+  //     imgurl: require("../../assets/Research/gate.png"),
+  //   },
+    // {
+    //   title: "Data-free Incremental Learning",
+    //   description: 
+    //   "A major challenge of incremental learning is catastrophic forgetting. To alleviate this problem, prior methodologies save a portion of training data that require perpetual storage, which may introduce privacy issues. Here, we propose a novel data-free class incremental learning framework that first synthesizes data from the model trained on previous classes to generate synthetic data, called Class Impression, which is used to update model with new data. With the assistance with serveral novel losses, our pipeline improves accuracy for class incremental learning.",
+    //   imgurl: require("../../assets/Research/class_impression.png"),
+    // },
 
-    {
-      title: "Explaining AI Models with Uncertainty",
-      description: "We propose DistDeepSHAP, a post-hoc feature importance estimation method under uncertainty evaluation for deep learning models.First, it can obtain uncertainty estimates for the provided feature importance scores. Second, it can better utilize the empirical distribution and has the potential for better feature importance scores estimation from the generated percentile saliency maps. Last but not least, it can compare with arbitrary subgroup references and interpret subgroup salient features, which is crucial for medical image study.",
-      imgurl: require("../../assets/Research/distdeepshap.png"),
-    },
+    // {
+    //   title: "Explaining AI Models with Uncertainty",
+    //   description: "We propose DistDeepSHAP, a post-hoc feature importance estimation method under uncertainty evaluation for deep learning models.First, it can obtain uncertainty estimates for the provided feature importance scores. Second, it can better utilize the empirical distribution and has the potential for better feature importance scores estimation from the generated percentile saliency maps. Last but not least, it can compare with arbitrary subgroup references and interpret subgroup salient features, which is crucial for medical image study.",
+    //   imgurl: require("../../assets/Research/distdeepshap.png"),
+    // },
   ];
 
   const renderProjects = () => {
