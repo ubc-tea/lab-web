d5201ddaa8e8980c71928176aafb8f2057897b2d
Nanboy-Ronan <jinruinan@163.com>
2025-03-08 21:12:11 -0800

    update previous members

--- src/LabContainer/Previous/Previous.js
+++ src/LabContainer/Previous/Previous.js
@@ -117,70 +117,10 @@ export default function People(props) {
   return (
     <div className="people-container screen-container" id={props.id || ""}>
       <div className="people-parent">
-        <ScreenHeading title={"People"} subHeading={""} />
+        <ScreenHeading title={"Previous Members"} subHeading={""} />
         {renderPeople()}
       </div>
     </div>
   );
   
-  // return (
-  //   <div>
-  //     <div className="people-parent">
-  //       <ScreenHeading title={"People"} subHeading={""} />
-  //     </div>
-
-  //     <div class="responsive">
-  //       <div class="gallery">
-  //         <a target="_blank" href="img_5terre.jpg">
-  //           <img
-  //             src="img_5terre.jpg"
-  //             alt="Cinque Terre"
-  //             width="600"
-  //             height="400"
-  //           />
-  //         </a>
-  //         <div class="desc">Add a description of the image here</div>
-  //       </div>
-  //     </div>
-
-  //     <div class="responsive">
-  //       <div class="gallery">
-  //         <a target="_blank" href="img_forest.jpg">
-  //           <img src="img_forest.jpg" alt="Forest" width="600" height="400" />
-  //         </a>
-  //         <div class="desc">Add a description of the image here</div>
-  //       </div>
-  //     </div>
-
-  //     <div class="responsive">
-  //       <div class="gallery">
-  //         <a target="_blank" href="img_lights.jpg">
-  //           <img
-  //             src="img_lights.jpg"
-  //             alt="Northern Lights"
-  //             width="600"
-  //             height="400"
-  //           />
-  //         </a>
-  //         <div class="desc">Add a description of the image here</div>
-  //       </div>
-  //     </div>
-
-  //     <div class="responsive">
-  //       <div class="gallery">
-  //         <a target="_blank" href="img_mountains.jpg">
-  //           <img
-  //             src="img_mountains.jpg"
-  //             alt="Mountains"
-  //             width="600"
-  //             height="400"
-  //           />
-  //         </a>
-  //         <div class="desc">Add a description of the image here</div>
-  //       </div>
-  //     </div>
-
-  //     <div class="clearfix"></div>
-  //   </div>
-  // );
 }
