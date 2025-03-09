5307a2d8f0b33f14d9e8f60ee93077ef54e057b4
Nanboy-Ronan <jinruinan@163.com>
2025-03-08 22:51:42 -0800

    gallery with scroll effect

--- src/LabContainer/Gallery/Gallery.js
+++ src/LabContainer/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import "./Gallery.css";
 
@@ -13,27 +13,14 @@ export default function Gallery(props) {
         require("../../assets/Home/bestflfm_www2024.jpg")
     ];
 
-    const [isPaused, setIsPaused] = useState(false);
-
-    useEffect(() => {
-        const circle = document.querySelector(".rotating-circle");
-        if (circle) {
-            circle.style.animationPlayState = isPaused ? "paused" : "running";
-        }
-    }, [isPaused]);
-
     return (
         <div className="gal-parent" id={props.id || ""}>
             <ScreenHeading title={"Gallery of TEA Group"} subHeading={""} />
 
-            <div className="rotating-gallery-container">
-                <div 
-                    className="rotating-circle" 
-                    onMouseEnter={() => setIsPaused(true)} 
-                    onMouseLeave={() => setIsPaused(false)}
-                >
-                    {images.map((src, index) => (
-                        <div key={index} className="rotating-image" style={{ backgroundImage: `url(${src})` }}></div>
+            <div className="scroll-gallery-container">
+                <div className="scroll-gallery">
+                    {[...images, ...images].map((src, index) => (
+                        <img key={index} src={src} alt={`Gallery image ${index + 1}`} />
                     ))}
                 </div>
             </div>
